128e9fa72e47fa5fdbed94fad4435be9cd1db7bb
luizfernando <luizfernandotic@gmail.com>
2020-02-14 19:07:39 -0300

    4_1 exercise 1

---
+++ exercises/4_1/exercise1.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 5;
+
+// adição
+console.log('Soma:',a+b);
+
+//subtração
+console.log('Subtração:',a-b);
+
+//multiplicação
+console.log('Multiplicação:',a*b);
+
+//divisão
+console.log('Divisão:',a/b);
+
+//Módulo retorna o resto da divisão
+console.log('Módulo:',a%b);
+
