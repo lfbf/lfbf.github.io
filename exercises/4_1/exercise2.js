89ae3da9eb74879ef36bd73b670de555298ee8cc
luizfernando <luizfernandotic@gmail.com>
2020-02-14 19:24:32 -0300

    4_1 exercise 2

---
+++ exercises/4_1/exercise2.js
@@ -0,0 +1,12 @@
+//2- Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores 
+//que serão comparados.
+
+let primeiroNumero = 5
+let segundoNumero = 3
+
+if ( primeiroNumero > segundoNumero) {
+  console.log('O maior número é:',primeiroNumero)
+}
+else {
+  console.log('O maior número é:',segundoNumero)
+}
