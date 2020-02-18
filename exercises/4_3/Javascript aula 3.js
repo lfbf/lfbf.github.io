7a3b90c274ecb99d48536d6e299b142aacc5d8d5
luizfernando <luizfernandotic@gmail.com>
2020-02-18 17:49:15 -0300

    4_3 Parte 1 exercise1 e Exercicio em aula

---
+++ exercises/4_3/Javascript aula 3.js
@@ -0,0 +1,36 @@
+
+
+// let carro = {
+//   placa: "QHY5320",
+//   modelo: "Tucson",
+//   marca: "Chevrolet"
+// };
+
+// console.log(carro.placa);
+
+
+// let carro = {
+//   placa: "QHY5320",
+//   modelo: "Tucson",
+//   marca: "Chevrolet",
+//   opcionais: {
+//     cambio: "automatico",
+//     combustivel: "Flex"
+//   }
+// };
+
+// console.log(carro.opcionais.cambio);
+
+// let carro = {
+//   placa: "QHY5320",
+//   modelo: "Tucson",
+//   marca: "Chevrolet",
+//   opcionais: {
+//     cambio: "automatico",
+//     combustivel: "Flex"
+//   },
+//   "outra-coisa": "carroextra"
+// };
+
+// console.log(carro["outra-coisa"]);
+
