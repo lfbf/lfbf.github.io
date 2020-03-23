6ce711ee9f62ac78226529a1ff5a4ef9d9ee692b
luizfernando <luizfernandotic@gmail.com>
2020-03-23 15:49:52 -0300

    8.3 exercício 1 corrigindo item 4

--- exercises/8_3/exercise_1.js
+++ exercises/8_3/exercise_1.js
@@ -28,3 +28,4 @@ assert.strictEqual(sum(4,5),9);
 assert.strictEqual(sum(0,0),0);
 
 assert.strictEqual(sum(4,'5'),9, 'Mensagem de erro personalizada soma 4 + 5 (string)' );
+assert.throws(() => {sum(4, '5')}, 'Error: parameters must be numbers');
